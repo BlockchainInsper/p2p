703f8fe92785ed8eb897c810486c967ba5a851eb
Bruno Arthur Cesconetto <brunoartc@yahoo.com>
2019-11-12 18:59:28 -0300

    Hash files (#1)
    * hash of files
    
    * refactor code

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 
 from flask import Flask, jsonify
 import hashlib
+from os import listdir
+from os.path import isfile, join
 
-files = ["bananinha quando nasce", "banana banana"]
+BUF_SIZE = 65536
 
 app = Flask(__name__) 
+
+
   
 @app.route('/files') 
 def hello_world(): 
-    return jsonify({'status':'success', 'data':[hashlib.sha256(i.encode('UTF-8')).hexdigest() for i in files]})
+    path = "./files"
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    resp = []
+
+    sha256 = hashlib.sha256()
+    
+    for current_file in onlyfiles:
+    
+        with open(path + "/" + current_file,'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha256.update(data)
+            resp.append({'name':current_file, 'hash':sha256.hexdigest()})
+    return jsonify({'status':'success', 'data':resp})
+
+
   
 if __name__ == '__main__': 
 
